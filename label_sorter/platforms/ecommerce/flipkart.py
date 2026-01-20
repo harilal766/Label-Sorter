621f43d5911ffab9fe59d1cafb17e78d583f45d4
harilal766 <harilalsunil2@gmail.com>
2026-01-20 17:06:12 +0530

    added module for flipkart labels

---
+++ label_sorter/platforms/ecommerce/flipkart.py
@@ -0,0 +1,15 @@
+import re
+from .base_label import BaseLabel
+
+class FlipkartLabel(BaseLabel):
+    def __init__(self, page_text, page_table, page_num):
+        super().__init__(page_text, page_table, page_num)
+        self.flipkart_order_id_pattern = r'OD\d{19}'
+        
+    def verify_page_is_not_cropped(self):
+        try:
+            pass
+        except Exception as e:
+            print(e)
+        
+        
