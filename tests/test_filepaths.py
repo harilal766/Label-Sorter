a659181095b20d9bc4fef4881ffb9faa22babe69
hari <hari@debian.16030454.xyz>
2026-06-13 18:51:29 +0530

    added code to generate json test file

--- tests/test_filepaths.py
+++ tests/test_filepaths.py
@@ -1,4 +1,4 @@
-import json, os
+import json, os, json
 from pathlib import Path
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -6,7 +6,23 @@ test_and_root_join = os.path.join(TEST_DIR,"..")
 Root = os.path.abspath(test_and_root_join)
 test_label_dir = os.path.join(Root,"test_labels")
 
-with open("label_sorter.json","r") as json_file:
+test_json_filename = "label_sorter.json"
+
+test_json_content = {
+    "before_sanitize" : "",
+    "after_sanitize" : ""
+}
+
+files = os.listdir()
+
+if not test_json_filename in files:
+    json_str = json.dumps(test_json_content, indent=4)
+    
+    with open(test_json_filename,"w") as json_file:
+        json_file.write(json_str)
+        json_file.close()
+
+with open(test_json_filename,"r") as json_file:
     credentials = json.load(json_file)
     
     amazon_pdf = os.path.join(test_label_dir, "amazon.pdf")
@@ -17,9 +33,6 @@ with open("label_sorter.json","r") as json_file:
     
     
     
-    
-print(test_label_dir)
-print(amazon_pdf)
-print(shopify_pdf)
+
     
 
