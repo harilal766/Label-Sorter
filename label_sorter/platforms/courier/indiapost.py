4d5f4c8541f768e0f3bab1a13105d500bf12c4f3
karmark <karmark@karmark.16030454.xyz>
2026-09-23 11:28:37 +0530

    added base class facilities for indiapost

--- label_sorter/platforms/courier/indiapost.py
+++ label_sorter/platforms/courier/indiapost.py
@@ -1,9 +1,28 @@
-from base_label import BaseLabel
+from label_sorter.platforms.base_label import BaseLabel
 
-
+import re
 
 
 class IndiapostLabel(BaseLabel):
     TRACKING_ID_PATTERN = r'EY\d{9}IN'
+    
+    def __init__(self, page_text, page_table = None, page_num = None):
+        super().__init__(page_text, page_table, page_num)
+    
+    
+    def get_pagetype(self):
+        type = None
+        if re.findall(self.TRACKING_ID_PATTERN,self.label_page_text):
+            type = self.PAGE_TYPES[0]
+        return type
+    
+    def get_page_summary(self) -> dict:
+        # Shipping Label
+        if self.get_pagetype() == self.PAGE_TYPES[0]:
+            self.tracking_id = self.extract_id("tracking")
+            
+            self.label_page_table = self.label_page_table[3]
+            return self.label_page_table
+    
     
     
--- tests/test_platforms/test_indiapost.py
+++ tests/test_platforms/test_indiapost.py
@@ -1,12 +1,37 @@
 from tests.test_platforms.test_baselabel import TestBaseLabel
-from label_sorter.platforms.courier.indiapost import IndiapostLabel
 from tests.test_filepaths import indiapost_pdf
+from label_sorter.platforms.courier.indiapost import IndiapostLabel
 
 import pdfplumber
 
 class TestIndiapost(TestBaseLabel):
     pdf = pdfplumber.open(indiapost_pdf)
     pages = pdf.pages
+    
+    testing_index = 0; test_page = pages[testing_index]
+    in_inst = IndiapostLabel(
+        page_text=test_page.extract_text(), page_table=test_page.extract_table(),
+        page_num=1
+    )
+    
+    def test_pages(self):
+        assert type(self.pages) == list
+        
+    def test_extract_id(self):
+        id = self.in_inst.extract_id(pattern_type="tracking")
+        print(id)
+        assert id
+        
+    def test_get_pagetype(self):
+        page_type = self.in_inst.get_pagetype()
+        print(page_type)
+        assert page_type == self.in_inst.PAGE_TYPES[0]
+        
+    def test_get_pagesummary(self):
+        summary = self.in_inst.get_page_summary()
+        print(summary)
+        
+        assert summary
 
     
     
